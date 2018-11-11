4303a76d510effc5695a7bff24197a815f0bc324
chang02 <chang0022@snu.ac.kr>
2018-11-11 21:14:51 +0900

    hw5

--- hw5/SM5_skel/translate.ml
+++ hw5/SM5_skel/translate.ml
@@ -9,11 +9,6 @@ module Translator = struct
 
   (* TODO : complete this function  *)
   let rec trans : K.program -> Sm5.command = function
-    | K.LETV (x, e1, e2) ->
-      trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
-      trans e2 @ [Sm5.UNBIND; Sm5.POP]
-    | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
-
     | K.NUM i -> [Sm5.PUSH (Sm5.Val (Sm5.Z i))]
     | K.TRUE -> [Sm5.PUSH (Sm5.Val (Sm5.B true))]
     | K.FALSE -> [Sm5.PUSH (Sm5.Val (Sm5.B false))]
@@ -31,6 +26,7 @@ module Translator = struct
 
     | K.ASSIGN (id, e) -> trans e @ [Sm5.PUSH (Sm5.Id id)] @ [Sm5.STORE] @ [Sm5.PUSH (Sm5.Id id)] @ [Sm5.LOAD]
     | K.SEQ (e1, e2) -> trans e1 @ [Sm5.POP] @ trans e2
+    | K.IF (e1, e2, e3) -> trans e1 @ [Sm5.JTR(trans e2, trans e3)]
     | K.WHILE (e, e1) -> 
         (
             let fname = "@fun@" in
