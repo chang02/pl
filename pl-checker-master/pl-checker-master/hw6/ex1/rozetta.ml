afd0bf5161fdef3c499d2a02e9cb3c23376c3c61
chang02 <chang0022@snu.ac.kr>
2018-11-27 23:20:36 +0900

    hw6

--- pl-checker-master/pl-checker-master/hw6/ex1/rozetta.ml
+++ pl-checker-master/pl-checker-master/hw6/ex1/rozetta.ml
@@ -16,11 +16,11 @@ let rec trans_obj : Sm5.obj -> Sonata.obj = function
   | Sm5.Id id -> Sonata.Id id
   | Sm5.Fn (arg, command) ->
     let tmp = "$fn" in
-    let body = [Sonata.MALLOC] @ [Sonata.BIND tmp] @ [Sonata.PUSH (Sonata.Id tmp)] @ [Sonata.STORE] @ 
-                trans' (command @ [Sm5.PUSH (Sm5.Id tmp)] @ [Sm5.LOAD] @
+    let body = [Sm5.MALLOC] @ [Sm5.BIND tmp] @ [Sm5.PUSH (Sm5.Id tmp)] @ [Sm5.STORE] @ 
+                command @ [Sm5.PUSH (Sm5.Id tmp)] @ [Sm5.LOAD] @
                         [Sm5.PUSH (Sm5.Val Sm5.Unit)] @ [Sm5.PUSH (Sm5.Id tmp)] @ [Sm5.UNBIND] @ [Sm5.POP] @
-                        [Sm5.CALL]) in
-    Sonata.Fn (arg, body)
+                        [Sm5.CALL] in
+    Sonata.Fn (arg, trans' body)
 
 (* TODO : complete this function *)
 and trans' : Sm5.command -> Sonata.command = function
